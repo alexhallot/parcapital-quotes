23b90c33335d06d1408f3dcec4caef8cf102809d
Alexandre <alexandre.hallot@gmail.com>
2026-03-26 16:50:28 -0300

    Initial commit: Flask stock quotes app (AAPL, META, PBR)

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import yfinance as yf
+
+app = Flask(__name__)
+
+STOCKS = ["AAPL", "META", "PBR"]
+
+@app.route("/")
+def index():
+    quotes = []
+    for symbol in STOCKS:
+        ticker = yf.Ticker(symbol)
+        info = ticker.fast_info
+        quotes.append({
+            "symbol": symbol,
+            "price": round(info.last_price, 2),
+            "currency": info.currency,
+        })
+    return render_template("index.html", quotes=quotes)
+
+if __name__ == "__main__":
+    app.run(debug=True)
